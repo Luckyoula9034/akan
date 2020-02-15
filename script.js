e2a05126541b0d483aec724515178bf998645de4
luqqy <luckyoula@gmail.com>
2020-02-15 11:36:13 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-var maleNames =["Kwasi","Kwando", "Kwabena", "kwaku","yaw", "kofi", "kwame"]
-var maleNames =["Akosua","Adwoa","Abenaa","Akua","Yaa","Ama"]
-var daysOfweek=["sunday","monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+
+function akan(){
+    var date = document.getElementById(date).value;
+    var month = document.getElementById(month).value;
+    var year=document.getElementById(year)
+
+
+if (date===""&&date<1&&date>31) {
+    alert("date between 1 and 31");
+    
+}
+else if(month===""&&month<1&&month>12){
+    alert("date must be between 1 and 12");
+}
+else if (year==="") {
+    alert("you must enter a value year"  );
+
+    
+}
+else{
+    return false;
+}
+}
+fun
