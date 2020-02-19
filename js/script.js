685df72ad8792c212a4923a18e3eff7099be2ae7
luqqy <luckyoula@gmail.com>
2020-02-19 11:19:44 +0300

    update

--- js/script.js
+++ js/script.js
@@ -24,6 +24,7 @@ function giveAkanName() {
   var gender = getGender();
   var date = new Date(year + "/" + month + "/" + day);
   var dayBorn = date.getDay();
+  var akanName;
   var validate = year > 0 && month > 0 && month <= 12 && day > 0 && day <= 31;
 
   if (year <= 0 || year > 2020) {
